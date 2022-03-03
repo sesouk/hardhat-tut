c2ad09ecb0a0101f6a9dd32aea1f4305a434b9a8
Kevin Tran <sesouk@gmail.com>
2022-03-02 22:17:15 -0700

    pre deployment

--- hardhat-tut/contracts/Whitelist.sol
+++ hardhat-tut/contracts/Whitelist.sol
@@ -1,21 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+
 contract Whitelist {
-  uint8 public maxWhitelistedAddresses;
+    uint8 public maxWhitelistedAddresses;
+    mapping(address => bool) public whitelistedAddresses;
+    uint8 public numAddressesWhitelisted;
 
-  mapping(address => bool) public whitelistedAddresses;
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+    }
 
-  uint8 public numAddressesWhitelisted;
+    function addAddressToWhitelist() public {
 
-  constructor(uint8 _maxWhitelistedAddresses) {
-    maxWhitelistedAddresses = _maxWhitelistedAddresses;
-  }
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        whitelistedAddresses[msg.sender] = true;
+        numAddressesWhitelisted += 1;
+    }
 
-  function addAddressToWhitelist() public {
-    require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
-    require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
-    whitelistedAddresses[msg.sender] = true;
-    numAddressesWhitelisted += 1;
-  }
 }
